e81d64a96f2188a42c59faaf2ae037cbb0e01f1a
etiennev <etiennev69@hotmail.fr>
2015-08-20 15:52:07 +0200

    ajout localstorage

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('ActivitesCtrl', function($scope, Activites, ionicMaterialInk) {
+.controller('ActivitesCtrl', function($scope, Activites, ionicMaterialInk, $ionicModal, $ionicPopup) {
       $scope.data = {};
 
       // With the new view caching in Ionic, Controllers are only called
@@ -13,26 +13,85 @@ angular.module('starter.controllers', [])
 
         ionicMaterialInk.displayEffect();
 
-      $scope.data.activites = Activites.all();
-      $scope.remove = function(activite) {
-        Activites.remove(activite);
-      };
+        $scope.data.activites = Activites.all();
+
+        $scope.data.addActivite = function () {
+            $scope.data.nomNewActivite = "";
+
+            $ionicPopup.show({
+                template: '<input type="text" ng-model="data.nomNewActivite">',
+                title: 'Nouvelle Activité',
+                scope: $scope,
+                buttons: [
+                    {text: 'Annuler'},
+                    {
+                        text: '<b>Confirmer</b>',
+                        type: 'button-positive',
+                        onTap: function (e) {
+                            if ($scope.data.nomNewActivite === '') {
+                                //don't allow the user to close unless he enters anumber
+                                e.preventDefault();
+                            } else {
+                                Activites.newActivite($scope.data.nomNewActivite);
+                            }
+                        }
+                    }
+                ]
+            });
+        };
+
+        $scope.remove = function(activite) {
+            Activites.remove(activite);
+        };
+
+
     })
 
-.controller('JeunesCtrl', function($scope, Jeunes) {
+.controller('JeunesCtrl', function($scope, Jeunes, $localstorage) {
       $scope.data = {};
 
       $scope.data.jeunes = Jeunes.all();
+
+        $scope.data.test = $localstorage.get('testval', 'init');
+
+        $scope.data.rafraichir = function () {
+            $localstorage.set('testval', $scope.data.test);
+        };
 })
 
-.controller('ActiviteDetailCtrl', function($scope, $stateParams, Activites, Jeunes, ionicMaterialInk) {
-      $scope.data = {};
+.controller('ActiviteDetailCtrl', function($scope, $stateParams, Activites, Jeunes, ionicMaterialInk, Presences) {
+        $scope.data = {};
 
-        ionicMaterialInk.displayEffect();
+        ionicMaterialInk.displayEffect(); // On active l'effet d'encre
 
-      $scope.data.jeunes = Jeunes.all();
+        $scope.data.jeunes = Jeunes.all(); // On récupère la liste des jeunes
 
-      $scope.data.activite = Activites.get($stateParams.activiteId);
+        for(var i = 0 ; i < $scope.data.jeunes.length ; i++)
+        {
+            $scope.data.jeunes[i].checked = Presences.getpresence($stateParams.activiteId, i);
+        }
+
+        $scope.data.activite = Activites.getActivite($stateParams.activiteId); // On récupère l'activité
+
+        $scope.data.getpresence = function (jeuneId) {
+            //return Activites.getpresence(activite, jeune);
+            return Presences.getpresence($stateParams.activiteId, jeuneId);
+        };
+
+        $scope.data.changepresence = function (jeune) {
+            //Activites.changepresence(activite, jeune);
+            //$scope.jeunes[jeune].checked = Activites.getpresence(activite, jeune);
+
+            if(Presences.getpresence($stateParams.activiteId, jeune) == true)
+            {
+                Presences.setpresence($stateParams.activiteId, jeune, false);
+            }
+            else
+            {
+                Presences.setpresence($stateParams.activiteId, jeune, true);
+            }
+        };
+
 })
 
 .controller('AccountCtrl', function($scope) {
--- www/js/services.js
+++ www/js/services.js
@@ -1,40 +1,67 @@
 angular.module('starter.services', [])
 
-    .factory('Activites', function() {
-      // Might use a resource here that returns a JSON array
+    .factory('Activites', function($localstorage) {
 
-      // Some fake testing data
-      var activites = [{
+      /*var activites = [{
         id: 0,
-        name: 'Réunion 18/06'
+        name: 'Réunion 18/06',
+        presences: {}
       }, {
         id: 1,
-        name: 'Réunion 9/09'
+        name: 'Réunion 9/09',
+        presences: {}
       }, {
         id: 2,
-        name: 'WE des passages'
+        name: 'WE des passages',
+        presences: {}
       }, {
         id: 3,
-        name: 'Réunion 30/09'
+        name: 'Réunion 30/09',
+        presences: {}
       }, {
         id: 4,
-        name: 'WE neige'
-      }];
+        name: 'WE neige',
+        presences: {}
+      }];*/
+
+
+      //var activites = $localstorage.getObject("activites"); // On recupère l'objet des activités
 
       return {
         all: function() {
-          return activites;
+          return $localstorage.getObject("activites");
         },
         remove: function(activite) {
+          var activites = $localstorage.getObject("activites");
+
           activites.splice(activites.indexOf(activite), 1);
         },
-        get: function(activiteId) {
+        getActivite: function(activiteId) {
+          var activites = $localstorage.getObject("activites");
+
           for (var i = 0; i < activites.length; i++) {
             if (activites[i].id === parseInt(activiteId)) {
               return activites[i];
             }
           }
           return null;
+        },
+        newActivite: function (nom) {
+          var activites = $localstorage.getObject("activites");
+
+          var count = 0;
+
+          // Comptage du nombre d'éléments dans le tableau
+          for(var prop in activites) {
+            if(activites.hasOwnProperty(prop))
+              ++count;
+          }
+
+          activites.push({
+            id: count,
+            name: nom,
+            presences: {}
+          });
         }
       };
     })
@@ -65,4 +92,51 @@ angular.module('starter.services', [])
       return jeunes;
     }
   };
-});
+})
+
+    .factory('Presences', function($localstorage, Activites) {
+
+      return {
+        setpresence: function(activiteId, jeuneId, value) {
+          var activites = $localstorage.getObject("activites"); // On recupère l'objet des activités
+
+          // On modifie l'objet
+          for (var i = 0; i < activites.length; i++) {
+            if (activites[i].id === parseInt(activiteId)) {
+              activites[i].presences[jeuneId] = value;
+            }
+          }
+
+          $localstorage.setObject("activites", activites); // on restocke l'objet
+        },
+        getpresence: function (activiteId, jeuneId) {
+          var activites = $localstorage.getObject("activites"); // On recupère l'objet des activités
+
+          for (var i = 0; i < activites.length; i++) {
+            if (activites[i].id === parseInt(activiteId)) {
+              if(typeof activites[i].presences[jeuneId] != 'undefined') {
+                return activites[i].presences[jeuneId];
+              }
+              else return false;
+            }
+          }
+        }
+      };
+    })
+
+    .factory('$localstorage', ['$window', function($window) {
+      return {
+        set: function(key, value) {
+          $window.localStorage[key] = value;
+        },
+        get: function(key, defaultValue) {
+          return $window.localStorage[key] || defaultValue;
+        },
+        setObject: function(key, value) {
+          $window.localStorage[key] = JSON.stringify(value);
+        },
+        getObject: function(key) {
+          return JSON.parse($window.localStorage[key] || '{}');
+        }
+      }
+    }]);
